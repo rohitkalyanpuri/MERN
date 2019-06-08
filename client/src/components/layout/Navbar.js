c0097e0efbf1b5962cdbc6a0e8ecf411a5df6b9d
rohitkalyanpuri <rohit_kalyanpuri@rediffmail.com>
2019-06-08 17:59:31 +0530

    navbar single changes
    navbar single changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -32,7 +32,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <Link to="index.html">
+        <Link to="/">
           <i className="fas fa-code" /> DevConnector
         </Link>
       </h1>
