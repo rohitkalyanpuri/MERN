5625e9194072978c40f04492824ece5c4b66123d
rohitkalyanpuri <rohit_kalyanpuri@rediffmail.com>
2019-06-09 20:27:09 +0530

    chapter 44 and 45
    chapter 44 and 45

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import alert from "../reducers/alert";
 import auth from "../reducers/auth";
+import profile from "../reducers/profile";
 export default combineReducers({
   alert,
-  auth
+  auth,
+  profile
 });
