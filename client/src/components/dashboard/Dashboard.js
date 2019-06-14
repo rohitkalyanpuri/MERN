5928df961a595b9d91d9c3335b7b288f9f3fcf6a
brohitkalyanpuri <rohit.kalyanpuri@bacancytechnology.com>
2019-06-14 19:29:47 +0530

    Edit Profile
    Edit Profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
+import DashboardAction from "./DashboardAction";
 
 const Dashboard = ({
   getCurrentProfile,
@@ -23,7 +24,9 @@ const Dashboard = ({
         <i className="fas fa-user">Welcome {user && user.name}</i>
       </p>
       {profile !== null ? (
-        <Fragment>has</Fragment>
+        <Fragment>
+          <DashboardAction />
+        </Fragment>
       ) : (
         <Fragment>
           <p>You have not yet setup a profile, please add some info</p>
---
+++ client/src/components/dashboard/DashboardAction.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const DashboardAction = props => {
+  return (
+    <div className="dash-buttons">
+      <Link to="/edit-profile" className="btn btn-light">
+        <i className="fas fa-user-circle text-primary" /> Edit Profile
+      </Link>
+      <Link to="/add-experience" className="btn btn-light">
+        <i className="fab fa-black-tie text-primary" /> Add Experience
+      </Link>
+      <Link to="/add-education" className="btn btn-light">
+        <i className="fas fa-graduation-cap text-primary" /> Add Education
+      </Link>
+    </div>
+  );
+};
+
+DashboardAction.propTypes = {};
+
+export default DashboardAction;
