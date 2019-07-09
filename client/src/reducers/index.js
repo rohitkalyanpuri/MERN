6d7dce9d37524666f9f9ee32088d5ac8857cf595
brohitkalyanpuri <rohit.kalyanpuri@bacancytechnology.com>
2019-07-09 13:40:48 +0530

    post reducers and actions
    post reducers and actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 import alert from "../reducers/alert";
 import auth from "../reducers/auth";
 import profile from "../reducers/profile";
+import post from "../reducers/post";
 export default combineReducers({
   alert,
   auth,
-  profile
+  profile,
+  post
 });
