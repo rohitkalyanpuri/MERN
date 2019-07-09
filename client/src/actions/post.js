207291c9345c2f8c78b80cabc094759269569bfd
brohitkalyanpuri <rohit.kalyanpuri@bacancytechnology.com>
2019-07-09 18:44:28 +0530

    POST Component Like and unlike
    POST Component Like and unlike

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 //GET Posts
 export const getPosts = () => async dispatch => {
@@ -9,6 +9,38 @@ export const getPosts = () => async dispatch => {
     dispatch({
       type: GET_POSTS,
       payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Add Like
+export const addLike = Id => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/like/${Id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { Id, likes: res.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Remove Like
+export const removeLike = Id => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${Id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { Id, likes: res.data }
     });
   } catch (err) {
     dispatch({
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,14 +1,30 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Spinner } from "../layout/Spinner";
+import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
 import { getPosts } from "../../actions/post";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div />;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user" />Welcome to the community
+      </p>
+      {/* PostForm */}
+      <div classNam="posts">
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
